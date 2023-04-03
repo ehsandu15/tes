ed09089dc02aa26db81def8de105cb0cfdeb8d73
ehsandu15 <ehsanduwidi@eh-du.net>
2023-04-03 18:23:38 +0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button, Container, Grid } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+      <Container>
+        <h1>Fix me ;</h1>
+      </Container>
+    </>
+  )
+}
